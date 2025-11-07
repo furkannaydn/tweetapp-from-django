6d6cd92584308215bebdfd150324c33466ef1524
furkan aydın <businessfurkan1@gmail.com>
2025-11-07 12:56:02 +0300

    admin features added

--- djangotweet/tweetapp/admin.py
+++ djangotweet/tweetapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
+from django.urls import path, include
+from tweetapp.models import Tweet
+
+
+class TweetAdmin(admin.ModelAdmin):
+    fieldsets =[
+        ('Message Group', {'fields': ['message']}),
+        ('Nick Group', {'fields': ['nickname']}),
+    ]
+    
+    #fields=['nickname', 'message']
 
 # Register your models here.
+admin.site.register(Tweet, TweetAdmin)
--- djangotweet/tweetapp/views.py
+++ djangotweet/tweetapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from . import models
+from django.urls import reverse
 
 # Create your views here.
 
@@ -12,6 +13,7 @@ def addtweet(request):
     if request.method == "POST":
         nickname = request.POST.get("nickname")
         message = request.POST.get("message")
-        print(f"Nickname: {nickname}, Message: {message}")
-
-    return render(request, 'tweetapp/addtweet.html') 
+        models.Tweet.objects.create(nickname=nickname, message=message)
+        return redirect(reverse('tweetapp:listtweet'))
+    else:
+        return render(request, 'tweetapp/addtweet.html')
